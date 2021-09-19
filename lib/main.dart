13c4c230aadc168323fcd34effd4b95ac513a4b6
Angga Baskara <yesangga@Anggas-MacBook-Pro.local>
2021-09-19 18:19:10 +0700

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_web/pages/landing_page.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: LandingPage(),
+    );
+  }
+}
